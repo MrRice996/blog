8fb98b74408ab037520e2fa97dea5473196182ea
fanyi <fanyi@htrdit.com>
2021-05-07 11:58:21 +0800

    update 删除大图 增加本地启动脚本

---
+++ stop.bat
@@ -0,0 +1,11 @@
+@echo off
+    setlocal enabledelayedexpansion
+    for /f "tokens=1-5" %%a in ('netstat -ano ^| find "4000"') do (
+        if "%%e%" == "" (
+            set pid=%%d
+        ) else (
+            set pid=%%e
+        )
+        echo !pid!
+        taskkill /f /pid !pid!
+    )
